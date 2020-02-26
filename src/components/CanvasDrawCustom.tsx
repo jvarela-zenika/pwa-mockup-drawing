345d82c6f8a28561b1c8eb82e4f11eb7263f563e
jvarela <jonathan.varela@zenika.com>
2020-02-26 17:42:47 +0100

    start

---
+++ src/components/CanvasDrawCustom.tsx
@@ -0,0 +1,58 @@
+import React, {FunctionComponent} from 'react';
+import './ExploreContainer.css';
+import {IonApp, IonButton, IonButtons, IonContent, IonIcon, IonRow, IonToolbar} from "@ionic/react";
+import {arrowRedoOutline, arrowUndoOutline, trashOutline} from "ionicons/icons";
+import CanvasDraw from "react-canvas-draw";
+
+interface ContainerProps {
+    name: string;
+}
+
+const CanvasDrawCustom: FunctionComponent = () => {
+
+    let canvas: CanvasDraw | null;
+
+    return (
+        <IonContent>
+            <IonRow>
+                <CanvasDraw
+                    ref={canvasDraw => (canvas = canvasDraw)}
+                    canvasWidth={window.innerWidth}
+                    canvasHeight={window.innerHeight-80}
+                    brushRadius={2}
+                    brushColor={"#000"}
+                    hideGrid={true}
+                />
+            </IonRow>
+            <IonRow>
+                <IonToolbar>
+                    <IonButtons slot={"start"}>
+                        <IonButtons>
+                            <IonIcon
+                                slot={"start"}
+                                size={"large"}
+                                icon={arrowUndoOutline}
+                                onClick={() => {
+                                    canvas?.undo()
+                                }}
+                            />
+                        </IonButtons>
+                    </IonButtons>
+                    <IonButtons slot={"end"}>
+                        <IonButton onClick={() => {
+                            canvas?.clear()
+                        }}>
+                            <IonIcon
+                                slot={"end"}
+                                size={"large"}
+                                icon={trashOutline}
+                            />
+                        </IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonRow>
+        </IonContent>
+    );
+};
+
+export default CanvasDrawCustom;
